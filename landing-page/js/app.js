e51d105c071aacccc6943ad3787a6cd83de92e7c
DerekPospiech <45837749+DerekPospiech@users.noreply.github.com>
2020-05-21 23:10:19 -0400

    Add files via upload

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -13,45 +13,54 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
+const sections = document.querySelectorAll('section'); //used for scroll event fucnction and buildNav
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+//decides if the element is in the viewport at all
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 ||
+        bounding.left >= 0 ||
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+//any time document is scrolled it checks what is into view and sets the active state
+document.addEventListener("scroll", function(){
+  for (const sect of sections) {
+    if(isInViewport(sect)){
+      sect.classList.add("your-active-class")
+    } else {
+      sect.classList.remove("your-active-class")
+    }
+  }
+});
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
+// builds the nav
 function buildNav(){
-
 const myDocFrag = document.createDocumentFragment();
-const sections = document.querySelectorAll('section');
 let i = 1; //used to update the href link in the innerhtml of the list item
 
-for (const section of sections) {
+for (const sect of sections) {
   const listItem = document.createElement('li');
-  const listItemInnerText = section.querySelector('h2').innerText;
-  listItem.innerHTML = "<a href='#section${i}'>${listItemInnerText}</a>"
+  const listItemInnerText = sect.querySelector('h2').innerText;
+  listItem.innerHTML = `<a href="#section${i}" class="header__link">${listItemInnerText}</a>`
+  listItem.addEventListener("Click", function () {
+    sect.scrollIntoView();
+  });
+
   myDocFrag.appendChild(listItem);
  i++;
 }
 
 //appends doc fragment to unordered list
-const navbarList = document.querySelector('navbar__list');
-navbarList.appendChild(myDocFrag);
+document.getElementById("navbar__list").appendChild(myDocFrag);
 }
+
+
 
 buildNav();
 
