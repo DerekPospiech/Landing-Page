2c0f4738da4fe625e619f1fcd34f037a8d2dfd6f
DerekPospiech <45837749+DerekPospiech@users.noreply.github.com>
2020-05-22 11:49:37 -0400

    Add files via upload

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -16,28 +16,6 @@
 const sections = document.querySelectorAll('section'); //used for scroll event fucnction and buildNav
 
 
-//decides if the element is in the viewport at all
-var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 ||
-        bounding.left >= 0 ||
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
-//any time document is scrolled it checks what is into view and sets the active state
-document.addEventListener("scroll", function(){
-  for (const sect of sections) {
-    if(isInViewport(sect)){
-      sect.classList.add("your-active-class")
-    } else {
-      sect.classList.remove("your-active-class")
-    }
-  }
-});
-
 
 // builds the nav
 function buildNav(){
@@ -49,7 +27,7 @@ for (const sect of sections) {
   const listItemInnerText = sect.querySelector('h2').innerText;
   listItem.innerHTML = `<a href="#section${i}" class="header__link">${listItemInnerText}</a>`
   listItem.addEventListener("Click", function () {
-    sect.scrollIntoView();
+    sect.scrollIntoView("smooth");
   });
 
   myDocFrag.appendChild(listItem);
@@ -60,9 +38,27 @@ for (const sect of sections) {
 document.getElementById("navbar__list").appendChild(myDocFrag);
 }
 
-
-
 buildNav();
+
+const links = document.querySelectorAll('.header__link'); //gets the links that were dynamiclly generated at the top of the page. must run after buildnav
+
+//any time document is scrolled it checks if a section is in view and sets the active state
+window.addEventListener("scroll", function(){
+  for (const sect of sections) {
+    if(sect.getBoundingClientRect().top <= this.window.innerHeight && sect.getBoundingClientRect().top > -20){
+      sect.classList.add("your-active-class")
+      for (link of links) {
+        if (link.innerText == sect.querySelector('h2').innerText) {
+          link.classList.add("your-active-section")
+        } else {
+          link.classList.remove("your-active-section")
+        }
+      }
+    } else {
+      sect.classList.remove("your-active-class")
+    }
+  }
+});
 
 // Add class 'active' to section when near top of viewport
 
